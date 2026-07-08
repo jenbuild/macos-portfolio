415f00f66697a4c5b5a709fb0d49e770d6110abb
Ajay Kumar <iajaymk+github@gmail.com>
2026-07-08 19:08:30 +0530

    Implement window hoc and terminal component

--- src/store/window.js
+++ src/store/window.js
@@ -3,38 +3,39 @@ import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 
 const useWindowStore = create(
-    immer((set) => ({
-    windows: WINDOW_CONFIG,
-    nextZIndex: INITIAL_Z_INDEX + 1,
+	immer((set) => ({
+		windows: WINDOW_CONFIG,
+		nextZIndex: INITIAL_Z_INDEX + 1,
 
-    openWindow: (windowKey, data = null) => 
-        set((state) => {
-        const win = state.windows[windowKey];
-        if(!win) return;
+		openWindow: (windowKey, data = null) =>
+			set((state) => {
+				const win = state.windows[windowKey];
+				if (!win) return;
 
-        win.isOpen = true;
-        win.zIndex = state.nextZIndex;
-        win.data = data ?? win.data;
-        state.nextZIndex++;
-    }),
+				win.isOpen = true;
+				win.zIndex = state.nextZIndex;
+				win.data = data ?? win.data;
+				state.nextZIndex++;
+			}),
 
-    closeWindow: (windowKey) => 
-        set((state) => {
-        const win = state.windows[windowKey];
-        if(!win) return;
+		closeWindow: (windowKey) =>
+			set((state) => {
+				const win = state.windows[windowKey];
+				if (!win) return;
 
-        win.isOpen = false;
-        win.zIndex = INITIAL_Z_INDEX;
-        win.data = null;
-    }),
+				win.isOpen = false;
+				win.zIndex = INITIAL_Z_INDEX;
+				win.data = null;
+			}),
 
-    focusWindow: (windowKey) => 
-        set((state) => {
-        const win = state.windows[windowKey];
-        if(!win) return;
-        
-        win.zIndex = state.nextZIndex++;
-    })
-})));
+		focusWindow: (windowKey) =>
+			set((state) => {
+				const win = state.windows[windowKey];
+				if (!win) return;
+
+				win.zIndex = state.nextZIndex++;
+			}),
+	})),
+);
 
 export default useWindowStore;
